759e98e891dbce937b42acab0dbdc46e35e42291
William Weir <williamweir10@gmail.com>
2023-10-20 20:37:12 -0400

    initial commit

---
+++ main_project/Home.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from pages.n1_Network_Tool import page1_funcs  # Import the dictionary of subpage functions
+from pages.n2_Password_Tools import page2_funcs # Import the dictionary of subpage functions
+from pages.n3_Plotting_Demo import page3
+from pages.n4_Mapping_Demo import page4
+from pages.n5_DataFrame_Demo import page5
+
+st.set_page_config(page_title="Shadow Suite", page_icon="🕸")
+
+page_names_to_funcs = {
+    "Main Page": "main_page",
+    "Network Tool": page1_funcs,  # Use the dictionary as the value for 'Network Tool'
+    "Password Tools": page2_funcs, # Use the dictionary as the value for 'Password Tools'
+    "Page 2": page3,
+    "Page 3": page4,
+    "Page 4": page5,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+
+if selected_page == "Main Page":
+    st.markdown(
+    """
+    # Welcome to Streamlit! 👋
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+elif isinstance(page_names_to_funcs[selected_page], dict):
+    # If the selected page has subpages
+    selected_subpage = st.sidebar.selectbox("Select a function", page_names_to_funcs[selected_page].keys())
+    page_names_to_funcs[selected_page][selected_subpage]()  # Run the selected subpage function
+else:
+    # If the selected page doesn't have subpages, just run the function
+    page_names_to_funcs[selected_page]()
---
+++ main_project/pages/n2_Password_Tools.py
@@ -0,0 +1,47 @@
+def ascii_animation():
+    import time
+    import streamlit as st
+
+    animation_text = r"""
+                             .';cloooolc:,.                                 
+                          .cx0NWMMMMMMMMWWXOo,.                             
+                        'dXWMMMMMMMMMMMMMMMMMNO:.                           
+                       cKMMMMMMMMMMMMMMMMMMMMMMWk'                          
+                      cXMMMMMMMMMMMMMMMMMMMMMMMMW0,                         
+                     ;KMMMMMMMMMMMMMMMMMMMMMMMMMMWk.                        
+                    .xWMMMMMMMMMMMMMMMMMMMMMMMMMMMX:                        
+                    ;KMMMMMMMMMMMMMMMMMMMMMMMMMMMMWd                        
+                    lWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMx.                       
+                   .dWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMk.                       
+                   .xMMWMMMMMMMMMMMMMMMMMMMMMMMMWWMk.                       
+                    oWOccokKWMMMMMMMMMMMMMWXkoc::kNd.                       
+                    ,K0'   .;dXMMMMMMMMMWOc.    ,0K;                        
+                     lXk.     .dNMMMMMMKc.     ;0No.                        
+                     .oX0:.     cXMMMMK:     'dXWx.                         
+                      .cXNOl,.   oNMMWo. .':xXMNd.                          
+                        ;0WMNKOxdkNMMNOxkKNWMMXo.                           
+                         .xNMMMMMMMMMMMMMMMMW0;                             
+                           :0WMMMMMMMMMMMMMXd.                              
+                            .c0WMMMMMMMMMXx,                                
+                              .;dOKXXX0xc.                                  
+                                 .....                                     
+    """
+    animation_placeholder = st.empty()
+    for i in range(len(animation_text)):
+        animation_placeholder.text(animation_text[:i])
+        time.sleep(0.001)
+    animation_placeholder.empty()
+
+def hash_cracker():
+    import streamlit as st
+
+    if st.sidebar.button("View ASCII Animation"):
+        ascii_animation()
+    
+
+# Dictionary of subpage functions
+page2_funcs = {
+    "Hash Cracker": hash_cracker
+    #"Network Analysis": network_analysis,
+    #"Subnet Calculator": subnet_calculator
+}
