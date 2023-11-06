e73c7396149669779a6abdb62655d0d89ad740dc
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-11-06 17:00:38 -0500

    Added subnet scanner tests Fixed typo in local testing port

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -11,7 +11,7 @@ from playwright.sync_api import Page, expect
 
 LOCAL_TEST = False
 
-PORT = "8503" if LOCAL_TEST else "8699"
+PORT = "8501" if LOCAL_TEST else "8699"
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -102,8 +102,104 @@ def test_certificate_lookup(page: Page):
 
 
 def test_subnet_scanner(page: Page):
-    # TODO: empty test
-    pass
+    def enter_ip(ip):
+        page.get_by_label("Enter IP address").click()
+        page.get_by_label("Enter IP address").fill(ip)
+        page.get_by_label("Enter IP address").press("Enter")
+        running_icon.wait_for(state="hidden")
+
+    page.get_by_role("img", name="open").click()
+    page.get_by_text("Network Tool").click()
+    page.get_by_role("img", name="open").nth(1).click()
+    page.get_by_text("Subnet Scanner").click()
+    running_icon = page.get_by_text("Running...")
+
+    # Invalid input - not IP
+    enter_ip("1.2.3")
+
+    # Check error message
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text("Invalid IP address.")
+
+
+    # Invalid input - bogon IP
+    enter_ip("192.168.0.1")
+
+    # Check error message
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text("That IP is reserved for special use and cannot be located.")
+
+
+    # Valid input - 8.8.8.8
+    enter_ip("8.8.8.8")
+
+    # Check map
+    expect(page.locator("#view-default-view")).to_be_visible()
+
+    # Check table
+    expect(page.locator(".dvn-scroller")).to_be_visible()
+    table = page.locator("//table[@role='grid']")
+
+    # Headers
+    EXPECTED_HEADERS = ["", "IP", "City", "Country"]
+    for col, expected in enumerate(EXPECTED_HEADERS, 1):
+        expect(table.locator(f"//thead/tr/th[@aria-colindex={col}]")).to_have_text(expected)
+    # Body
+    for row_num in range (2, 256 + 2):  # Header included in numbering, rows start at 2
+        row = table.locator(f"//tbody/tr[@aria-rowindex={row_num}]")
+
+        expect(row.locator("//td[@aria-colindex=1]")).to_have_text(f"{row_num - 2}")
+        expect(row.locator("//td[@aria-colindex=2]")).to_have_text(f"8.8.8.{row_num - 2}")
+
+        if row_num - 2 >= 4 and row_num - 2 <= 7:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Ath Thawrah")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("SY")
+        elif (row_num - 2 >= 16 and row_num - 2 <= 19) or \
+            row_num - 2 == 84 or \
+            row_num - 2 == 115 or \
+            row_num - 2 == 138 or \
+            (row_num - 2 >= 204 and row_num - 2 <= 207) or \
+            (row_num - 2 >= 242 and row_num - 2 <= 243):
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Pretty Prairie")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("US")
+        elif (row_num - 2 >= 20 and row_num - 2 <= 23) or \
+            (row_num - 2 >= 86 and row_num - 2 <= 87) or \
+            row_num - 2 == 116 or \
+            (row_num - 2 >= 148 and row_num - 2 <= 149) or \
+            (row_num - 2 >= 188 and row_num - 2 <= 195) or \
+            (row_num - 2 >= 216 and row_num - 2 <= 223) or \
+            row_num - 2 == 235:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Mecca")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("SA")
+        elif row_num - 2 == 29:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Lucknow")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("IN")
+        elif row_num - 2 >= 44 and row_num - 2 <= 47:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Jambi City")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("ID")
+        elif row_num - 2 >= 100 and row_num - 2 <= 101:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Narsimhapur")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("IN")
+        elif row_num - 2 >= 102 and row_num - 2 <= 103:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Wed Alnkil")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("SA")
+        elif row_num - 2 >= 104 and row_num - 2 <= 111:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Ar Rass")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("SA")
+        elif row_num - 2 == 114:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Chicago")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("US")
+        elif row_num - 2 >= 128 and row_num - 2 <= 131:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Dardenne Prairie")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("US")
+        elif row_num - 2 >= 160 and row_num - 2 <= 161:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Hyderābād")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("IN")
+        else:
+            expect(row.locator("//td[@aria-colindex=3]")).to_have_text("Mountain View")
+            expect(row.locator("//td[@aria-colindex=4]")).to_have_text("US")
 
 
 def test_wget(page: Page):
