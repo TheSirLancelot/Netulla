65a458afbc1ba3014478d1f5ae9fbb12795066ff
Oscar Vasquez <48740780+oavf15@users.noreply.github.com>
2023-11-23 12:49:41 -0500

    Revert "update test again"
    This reverts commit 0896b0396d9c82cb0daee7bbab88e516d8493f0a.

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -42,8 +42,61 @@ def test_url_encoder_decoder(page: Page):
 
 
 def test_http_header_tool(page: Page):
-    # TODO: empty test
-    pass
+    def enter_address(address):
+        page.get_by_label("Enter URL or IP address").click()
+        page.get_by_label("Enter URL or IP address").fill(address)
+        page.get_by_test_id("baseButton-secondary").click()
+        page.get_by_text("Running...").wait_for(state="hidden")
+
+    page.frame_locator("iframe[title=\"streamlit_antd_components\\.utils\\.component_func\\.sac\"]").get_by_role("menuitem", name=" HTTP Header Tool").click()
+
+    # Check page title
+    expect(page.get_by_role("heading", name="HTTP Header Tool").locator("span")).to_be_visible()
+
+    # Check invalid inputs
+    enter_address("www.google.com")   # Missing schema
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text(
+        "Incomplete URL or invalid IP. Please include http:// or https:// for URLs, and enter IPs in the form x.x.x.x using only numbers."
+    )
+
+    enter_address("htt://www.google.com")   # Invalid schema
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text(
+        "Invalid URL. Please use http:// or https://"
+    )
+
+    enter_address("https://www.notasite.com")   # Invalid URL, disabled timeout b/c sometimes webkit test checks slightly before loaded
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text(
+        "Site doesn't exist or connection cannot be made at this time.",
+    timeout=0
+    )
+
+    enter_address("8.8.8")   # Invalid IP - wrong length
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text(
+        "Incomplete URL or invalid IP. Please include http:// or https:// for URLs, and enter IPs in the form x.x.x.x using only numbers."
+    )
+
+    enter_address("8.8.8.8s")   # Invalid IP - invalid characters
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text(
+        "Incomplete URL or invalid IP. Please include http:// or https:// for URLs, and enter IPs in the form x.x.x.x using only numbers."
+    )
+
+    # Check entering URL
+    enter_address("https://www.google.com")
+    expect(page.get_by_text("Headers")).to_be_visible()
+
+    # Check entering IP
+    enter_address("8.8.8.8")
+    expect(page.get_by_text("Headers")).to_be_visible()
 
 
 def test_reverse_ip(page: Page):
@@ -57,8 +110,62 @@ def test_certificate_lookup(page: Page):
 
 
 def test_subnet_scanner(page: Page):
-    # TODO: empty test
-    pass
+    def enter_ip(ip):
+        page.get_by_label("Enter IP address").click()
+        page.get_by_label("Enter IP address").fill(ip)
+        page.get_by_label("Enter IP address").press("Enter")
+        page.get_by_text("Running...").wait_for(state="hidden")
+
+    page.frame_locator("iframe[title=\"streamlit_antd_components\\.utils\\.component_func\\.sac\"]").get_by_role("menuitem", name=" Subnet Scanner").click()
+
+    # Invalid input - not IP
+    enter_ip("1.2.3")
+
+    # Check error message
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text("Invalid IP address.")
+
+    # Invalid input - bogon IP
+    enter_ip("192.168.0.1")
+
+    # Check error message
+    error = page.get_by_test_id("stNotification")
+    expect(error).to_be_visible()
+    expect(error).to_have_text(
+        "That IP is reserved for special use and cannot be located."
+    )
+
+    # Valid input - 8.8.8.8
+    enter_ip("8.8.8.8")
+
+    # Check map
+    # TODO: Make test work for checking map, currently hangs forever b/c map never appears in GitHub
+    # ip_map = page.locator("#view-default-view")
+    # expect(ip_map).to_be_visible()
+
+    # Check table
+    expect(page.locator(".dvn-scroller")).to_be_visible()
+    table = page.locator("//table[@role='grid']")
+
+    # Headers
+    EXPECTED_HEADERS = ["", "IP", "City", "Country"]
+    for col, expected in enumerate(EXPECTED_HEADERS, 1):
+        expect(table.locator(f"//thead/tr/th[@aria-colindex={col}]")).to_have_text(
+            expected
+        )
+    # Body
+    for row_num in range(2, 256 + 2):  # Header included in numbering, rows start at 2
+        row = table.locator(f"//tbody/tr[@aria-rowindex={row_num}]")
+
+        expect(row.locator("//td[@aria-colindex=1]")).to_have_text(f"{row_num - 2}")
+        expect(row.locator("//td[@aria-colindex=2]")).to_have_text(
+            f"8.8.8.{row_num - 2}"
+        )
+
+        # Location data may change, just check that cells are full
+        expect(row.locator("//td[@aria-colindex=3]")).not_to_be_empty()
+        expect(row.locator("//td[@aria-colindex=4]")).not_to_be_empty()
 
 
 def test_wget(page: Page):
@@ -72,40 +179,8 @@ def test_password_complexity(page: Page):
 
 
 def test_ns_lookup(page: Page):
-    # Go to the main page of the Streamlit app
-    page.goto(f"http://localhost:{PORT}")
-
-    # Click on the Ns Lookup tool
-    page.frame_locator("iframe[title=\"streamlit_antd_components\\.utils\\.component_func\\.sac\"]").get_by_role("menuitem", name=" Ns Lookup").click()
-
-    # Wait for the input field to be visible on the ns_lookup subpage
-    domain_input_selector = 'input[aria-label="Enter Domain (e.g., google.com)"]'
-    domain_input = page.wait_for_selector(domain_input_selector, state="visible")
-    
-
-    # Fill in the known domain name
-    domain_input.fill("google.com")
-
-    # Simulate pressing the Enter key to submit the domain
-    page.press(domain_input_selector, "Enter")
-
-    # Wait for the Streamlit action to complete
-    page.wait_for_timeout(3000)  # Adjust this based on response time
-
-    # Check for the success message
-    success_message = page.locator("text=Valid Domain")
-    expect(success_message).to_have_count(1)
-
-    # Check if IP addresses are displayed as a list
-    ip_addresses = page.locator("ul > li")
-    ip_count = ip_addresses.count()
-
-    # Assert that there is at least one IP address
-    assert ip_count > 0, "No IP addresses were found."
-
-    # Check if the hostname is displayed
-    hostname = page.locator("text=Hostname")
-    expect(hostname).to_have_count(1)
+    # TODO: empty test
+    pass
 
 
 def test_ping(page: Page):
