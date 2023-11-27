a2be0a314d38d131d90599a366afccfdad2134aa
William Weir <williamweir10@gmail.com>
2023-11-26 21:06:35 -0500

    Update test_frontend.py

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -253,7 +253,7 @@ def test_whois_lookup(page: Page):
         page.get_by_label("Enter URL or IP address").fill(address)
         page.get_by_test_id("baseButton-secondary").click()
 
-    # Go to NS Lookup function
+    # Go to Whois Lookup function
     page.frame_locator(
         'iframe[title="streamlit_antd_components\\.utils\\.component_func\\.sac"]'
     ).get_by_role("menuitem", name=" Whois Lookup").click()
